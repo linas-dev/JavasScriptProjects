6c64fbb965005ce39b13bbd90791f771ac7bc96b
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-11-06 15:11:30 +0000

    jquery

---
+++ jQuery-Projects/jq.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("html").append(
+    "<body>\
+      <div id='container' >\
+        <div id='inner' >\
+          <h1>Linas</h1>\
+        </div>\
+      </div>\
+    </body>"
+  );
+});
