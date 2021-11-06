c95aba0b0c049444137aba9685f161ece1a26df1
Linas Smita <linassmita@Linass-MacBook-Air.local>
2021-11-06 16:27:27 +0000

    jquery udpate

--- jQuery-Projects/jq.js
+++ jQuery-Projects/jq.js
@@ -3,9 +3,16 @@ $(document).ready(function() {
     "<body>\
       <div id='container' >\
         <div id='inner' >\
-          <h1>Linas</h1>\
+          <h1>Basic jQuery</h1>\
+          <p id='1'>Paragraph 1</p>\
+          <p id='2'>Paragraph 2</p>\
+          <p id='3'>Paragraph 3</p>\
         </div>\
       </div>\
     </body>"
   );
+  $("#2").css({"color":"red"});
+  $("#3").click(function() {
+    $('h1').hide().fada();
+  })
 });
